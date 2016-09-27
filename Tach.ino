79110501d33e6659c87ab5d3d5bba2bc9364a580
renat2985 <wul@inbox.lv>
2016-09-27 12:23:33 +0300

    Small fix

--- Tach.ino
+++ Tach.ino
@@ -2,7 +2,7 @@
 void Tach_0() {
  static unsigned long millis_prev;
  // Устроняем дребезг контакта
- if (millis() - 100 > millis_prev) {
+ if (millis() - 500 > millis_prev) {
   chaing = 1; // Выстовляем признак нажатия кнопки
  }
  millis_prev = millis();
@@ -12,14 +12,13 @@ void Time01() {
   tickerSetLow.attach(TimeLed, setT1, 0);
   LedON(r, g, b);
   state0=3;
-     analogWrite(buzer_pin, volume);
-
-
+  analogWrite(buzer_pin, volume);
 }
 
 void setT1(int state) {
- tickerSetLow.detach();
+  tickerSetLow.detach();
   analogWrite(buzer_pin, 0);
   digitalWrite(buzer_pin,1);
+  chaing = 1;
 }
 
